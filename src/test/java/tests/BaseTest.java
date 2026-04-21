1a918bf5465b0a7d1ddd380a709888d1ef9e64c1
Denis <dafinoge40@gmail.com>
2026-04-21 15:57:31 +0300

    +

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriver driver = new ChromeDriver();
+
+    }
+
+    @AfterMethod
+    public void close() {
+   driver.quit();
+    }
+}
